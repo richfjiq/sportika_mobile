debfc6aaaa6f98e8866add82cb758b3c19c6a076
richfjiq <rfjiq1986@gmail.com>
2023-02-06 20:43:14 -0600

    fix: add styles on width screens > 480 in MyOrders component

--- src/components/MyOrders/MyOrders.tsx
+++ src/components/MyOrders/MyOrders.tsx
@@ -1,12 +1,14 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, useWindowDimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { useOrders } from '../../store';
 import { colors } from '../../theme/appTheme';
 import { OrdersTable } from '../OrdersTable';
 import { styles } from './MyOrders.style';
+import { responsiveFontSize, responsiveIcon } from '../../utils';
 
 const MyOrders = () => {
+	const { width } = useWindowDimensions();
 	const { allOrders } = useOrders();
 	const pendingPayment = (allOrders || []).find((order) => order.isPaid === false);
 	const orders = allOrders || [];
@@ -15,28 +17,35 @@ const MyOrders = () => {
 		<ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
 			{pendingPayment && (
 				<View style={styles.warningContainer}>
-					<Text style={styles.warningText}>You only have 48 hours to pay your order.</Text>
+					<Text style={{ ...styles.warningText, ...responsiveFontSize(12, width) }}>
+						You only have 48 hours to pay your order.
+					</Text>
 				</View>
 			)}
 			{orders.length === 0 ? (
 				<View style={styles.noOrders}>
-					<Icon name={'reader-outline'} size={25} color={colors.black} />
-					<Text style={styles.noOrdersText}> No Orders</Text>
+					<Icon name={'reader-outline'} size={responsiveIcon(25, width)} color={colors.black} />
+					<Text style={{ ...styles.noOrdersText, ...responsiveFontSize(18, width) }}>
+						{' '}
+						No Orders
+					</Text>
 				</View>
 			) : (
 				<>
 					<View style={styles.rowContainer}>
 						<View style={styles.orderContainer}>
-							<Text style={styles.headerText}>Order</Text>
+							<Text style={{ ...styles.headerText, ...responsiveFontSize(14, width) }}>Order</Text>
 						</View>
 						<View style={styles.statusContainer}>
-							<Text style={styles.headerText}>Status</Text>
+							<Text style={{ ...styles.headerText, ...responsiveFontSize(14, width) }}>Status</Text>
 						</View>
 						<View style={styles.dateContainer}>
-							<Text style={styles.headerText}>Date</Text>
+							<Text style={{ ...styles.headerText, ...responsiveFontSize(14, width) }}>Date</Text>
 						</View>
 						<View style={styles.detailsContainer}>
-							<Text style={styles.headerText}>Details</Text>
+							<Text style={{ ...styles.headerText, ...responsiveFontSize(14, width) }}>
+								Details
+							</Text>
 						</View>
 					</View>
 					<OrdersTable />
