1d5bbf0376acbf1a3378cbee6a7508fa388108a1
richfjiq <rfjiq1986@gmail.com>
2023-02-06 21:03:15 -0600

    fix: add styles on width screens > 480 in PaymentStatus component

--- src/components/PaymentStatus/PaymentStatus.tsx
+++ src/components/PaymentStatus/PaymentStatus.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native';
+import { View, Text, useWindowDimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useOrders } from '../../store';
 
-import { colors } from '../../theme/appTheme';
 import { styles } from './PaymentStatus.style';
+import { colors } from '../../theme/appTheme';
+import { responsiveFontSize, responsiveIcon } from '../../utils';
 
 interface Props {
 	isPaid: boolean;
@@ -11,6 +12,7 @@ interface Props {
 }
 
 const PaymentStatus = ({ isPaid = false, timeInDays = 0 }: Props) => {
+	const { width } = useWindowDimensions();
 	const { order } = useOrders();
 
 	if (!order) return null;
@@ -18,16 +20,32 @@ const PaymentStatus = ({ isPaid = false, timeInDays = 0 }: Props) => {
 	if (timeInDays >= 2 && !isPaid) {
 		return (
 			<View style={styles.statusCanceled}>
-				<Icon name={'alert-circle-outline'} size={25} color={colors.greyText} />
-				<Text style={styles.textCanceled}>Order Canceled</Text>
+				<Icon
+					name={'alert-circle-outline'}
+					size={responsiveIcon(25, width)}
+					color={colors.greyText}
+				/>
+				<Text style={{ ...styles.textCanceled, ...responsiveFontSize(14, width) }}>
+					Order Canceled
+				</Text>
 			</View>
 		);
 	}
 
 	return (
 		<View style={isPaid ? styles.statusPaid : styles.statusPending}>
-			<Icon name={'card-outline'} size={25} color={isPaid ? colors.green : colors.red} />
-			<Text style={isPaid ? styles.textPaid : styles.textPending}>
+			<Icon
+				name={'card-outline'}
+				size={responsiveIcon(25, width)}
+				color={isPaid ? colors.green : colors.red}
+			/>
+			<Text
+				style={
+					isPaid
+						? { ...styles.textPaid, ...responsiveFontSize(14, width) }
+						: { ...styles.textPending, ...responsiveFontSize(14, width) }
+				}
+			>
 				{isPaid ? 'Order Paid' : 'Pending Payment'}
 			</Text>
 		</View>
