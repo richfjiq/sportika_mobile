9266446c67a426b4040ceb93ab6f19bca896b3e4
richfjiq <rfjiq1986@gmail.com>
2023-02-06 18:27:37 -0600

    fix: add styles on width screens > 480 in EmptyCart component

--- src/components/EmptyCart/EmptyCart.tsx
+++ src/components/EmptyCart/EmptyCart.tsx
@@ -1,12 +1,14 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, useWindowDimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { colors } from '../../theme/appTheme';
 import { Header } from '../Header';
 import { styles } from './EmptyCart.style';
+import { responsiveFontSize, responsiveIcon } from '../../utils';
 
 const EmptyCart = () => {
+	const { width } = useWindowDimensions();
 	const navigator = useNavigation();
 
 	const goToBuy = () => {
@@ -17,11 +19,15 @@ const EmptyCart = () => {
 		<View>
 			<Header title="Shopping Cart" search={false} />
 			<View style={styles.bodyContainer}>
-				<Icon name={'cart-outline'} size={50} color={colors.black} />
-				<Text>The cart is empty</Text>
+				<Icon name={'cart-outline'} size={responsiveIcon(50, width)} color={colors.black} />
+				<Text style={{ ...responsiveFontSize(12, width) }}>The cart is empty</Text>
 				<TouchableOpacity style={styles.button} activeOpacity={0.7} onPress={goToBuy}>
-					<Text style={styles.buttonText}>Buy now</Text>
-					<Icon name={'arrow-forward-outline'} size={20} color={colors.white} />
+					<Text style={{ ...styles.buttonText, ...responsiveFontSize(14, width) }}>Buy now</Text>
+					<Icon
+						name={'arrow-forward-outline'}
+						size={responsiveIcon(20, width)}
+						color={colors.white}
+					/>
 				</TouchableOpacity>
 			</View>
 		</View>
