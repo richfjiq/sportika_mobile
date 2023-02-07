4f2043482a1b7f9172eeda3911d26274243cef43
richfjiq <rfjiq1986@gmail.com>
2023-02-06 18:54:24 -0600

    fix: add styles on width screens > 480 in ItemQuantity component

--- src/components/ItemQuantity/ItemQuantity.tsx
+++ src/components/ItemQuantity/ItemQuantity.tsx
@@ -1,8 +1,9 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, useWindowDimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-import { colors } from '../../theme/appTheme';
 import { styles } from './ItemQuantity.style';
+import { colors } from '../../theme/appTheme';
+import { responsiveFontSize, responsiveIcon } from '../../utils';
 
 interface Props {
 	quantity: number;
@@ -11,6 +12,7 @@ interface Props {
 }
 
 const ItemQuantity = ({ quantity, updateQuantity, cart = false }: Props) => {
+	const { width } = useWindowDimensions();
 	const updateItemQuantity = (value: number) => {
 		if (value === -1) {
 			if (quantity === 1) return;
@@ -22,14 +24,38 @@ const ItemQuantity = ({ quantity, updateQuantity, cart = false }: Props) => {
 
 	return (
 		<>
-			<Text style={cart ? styles.subHeaderCart : styles.subHeader}>Quantity</Text>
+			<Text
+				style={
+					cart
+						? { ...styles.subHeaderCart, ...responsiveFontSize(14, width) }
+						: { ...styles.subHeader, ...responsiveFontSize(16, width) }
+				}
+			>
+				Quantity
+			</Text>
 			<View style={cart ? styles.quantityContainerCart : styles.quantityContainer}>
 				<TouchableOpacity onPress={() => updateItemQuantity(-1)}>
-					<Icon name={'remove-circle-outline'} size={cart ? 20 : 25} color={colors.black} />
+					<Icon
+						name={'remove-circle-outline'}
+						size={cart ? responsiveIcon(20, width) : responsiveIcon(25, width)}
+						color={colors.black}
+					/>
 				</TouchableOpacity>
-				<Text style={cart ? styles.quantityCart : styles.quantity}>{quantity}</Text>
+				<Text
+					style={
+						cart
+							? { ...styles.quantityCart, ...responsiveFontSize(14, width) }
+							: { ...styles.quantity, ...responsiveFontSize(18, width) }
+					}
+				>
+					{quantity}
+				</Text>
 				<TouchableOpacity onPress={() => updateItemQuantity(1)}>
-					<Icon name={'add-circle-outline'} size={cart ? 20 : 25} color={colors.black} />
+					<Icon
+						name={'add-circle-outline'}
+						size={cart ? responsiveIcon(20, width) : responsiveIcon(25, width)}
+						color={colors.black}
+					/>
 				</TouchableOpacity>
 			</View>
 		</>
